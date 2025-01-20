cff579fc9c3506a14326936fe6c622a29df2f0b9
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-01-20 17:12:21 -0500

    create house

--- src/lesson-scripts/15-shadows.js
+++ src/lesson-scripts/15-shadows.js
@@ -3,6 +3,16 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
 /**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader()
+const bakedShadow = textureLoader.load('/textures/shadows/bakedShadow.jpg')
+
+bakedShadow.colorSpace = THREE.SRGBColorSpace
+
+const simpleShadow = textureLoader.load('/textures/shadows/simpleShadow.jpg')
+/**
  * Base
  */
 // Debug
@@ -17,13 +27,13 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-// Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+// [Ambient light]
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
 gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 scene.add(ambientLight)
 
-// Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5)
+// [Directional light]
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -59,6 +69,53 @@ const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.sha
 directionalLightCameraHelper.visible = false
 scene.add(directionalLightCameraHelper)
 
+
+// [SpotLight]
+
+const spotLight = new THREE.SpotLight(0xffffff, 4, 10, Math.PI * 0.3)
+spotLight.castShadow = true
+spotLight.position.set(0, 2, 2)
+
+scene.add(spotLight)
+
+gui.add(spotLight, 'intensity').min(0.1).max(10).step(0.001).name('spotLight intensity')
+
+
+// optimizing shadow map by controlling mapSize
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024
+
+
+// toggling amplitude to optimize the shadow map
+spotLight.shadow.camera.fov = 30 // this seems to get overwritten by angle of new THREE.SpotLight
+
+// toggling near and far
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
+
+// this needs to be instantiated after toggling shadow camera property otherwise the change won't be reflected on ui
+// const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+// scene.add(spotLightCameraHelper)
+
+
+// [PointLight]
+const pointLight = new THREE.PointLight(0xffffff, 2.7)
+pointLight.castShadow = true
+
+pointLight.position.set(-1, 1, 0)
+scene.add(pointLight)
+
+
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.mapSize.height = 1024
+
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 4
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+pointLightCameraHelper.visible = false
+// PointLight shadow camera uses PerspectiveCamera but in all 6 directions and finishes downward (so cameraHelper always will look downward) 
+scene.add(pointLightCameraHelper)
 /**
  * Materials
  */
@@ -75,14 +132,60 @@ const sphere = new THREE.Mesh(
     material
 )
 
+// [Using Three.js real time shadow]
+// To get real-time shadows in three.js, we must use a material that supports shadows (e.g., MeshStandardMaterial, MeshPhongMaterial, etc.).
+
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(5, 5),
+//     material
+// )
+
+
+// [Using baked shadow: option 1]
+// If we use baked shadows instead, we can skip real-time shadow calculations
+// and use MeshBasicMaterial, which is more performant, as it simply displays the baked shadow texture.
+
+// const plane = new THREE.Mesh(
+//     new THREE.PlaneGeometry(5, 5),
+//     new THREE.MeshBasicMaterial({
+//         map: bakedShadow
+//     })
+// )
+
+// plane.position.x = 1 // if we move the object, shadow is coming from texture so the shadow can not follow the position of the object
+
+// [Using baked shadow: option 2]
+// we create extra mesh/object and we add shadow texture there, and moves it when the object moves so we can "mimic" the shadow follows the object
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
     material
 )
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5, 1.5),
+    new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        transparent: true, // to use alpha map, we have to make transparent prop as true
+        alphaMap: simpleShadow
+    })
+)
+
+
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
+
+sphereShadow.rotation.x = - Math.PI * 0.5 // this moves plane 90 degrees (it will be horizontally flat)
+// sphereShadow.position.y = plane.position.y // the same number as plane.position.y will cause z-fighting
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphereShadow)
+
+
+
 scene.add(sphere, plane)
+
+
 
 sphere.castShadow = true
 plane.receiveShadow = true
@@ -138,8 +241,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // we also need to 
 // 1. go through each object and toggle if it can cast a shadow with `castShadow` and if it can receive shadow with `receiveShadow`
 // 2. active the shadow on the lights
-renderer.shadowMap.enabled = true
-
+// renderer.shadowMap.enabled = true
+renderer.shadowMap.enabled = false // without commenting out all the shadow related code, we can disable shadow just by adding this line
 // we can change the shadowMap algorithm 
 console.log(renderer.shadowMap.type)
 // renderer.shadowMap.type = THREE.PCFSoftShadowMap // PCFSoftShadowMap won't enable radius (blur)
@@ -150,6 +253,27 @@ const clock = new THREE.Clock()
 
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+
+
+
+    // moves as circle
+
+    // Math.cos return the number from -1 to 1 and we are multiplying by 1.5 so sphere can travel from -1.5 to 1.5
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    // by multiplying elapsedTime by 3, the Math.sin function completes more cycles in the same time pf time, causing the object's movement to appear faster and more frequent
+    // by wrapping Math.abs, it will return the positive number all the times, enabling to mimic the bouncing effect
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    // updateShadow
+    sphereShadow.position.x = Math.cos(elapsedTime) * 1.5
+    sphereShadow.position.z = Math.sin(elapsedTime) * 1.5
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.3
+
+    const scale = Math.max(1 - Math.abs(sphere.position.y), 0.6)
+    sphereShadow.scale.set(scale, scale, scale)
+
+
 
     // Update controls
     controls.update()
---
+++ src/lesson-scripts/16-hounted-house.js
@@ -0,0 +1,194 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Timer } from 'three/addons/misc/Timer.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * House
+ */
+
+const houseGroup = new THREE.Group()
+
+
+// Wall 
+
+const wall = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial()
+)
+
+wall.position.y = 2.5 / 2
+
+
+// Roof 
+
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1.5, 4),
+    new THREE.MeshStandardMaterial()
+)
+roof.rotation.y = Math.PI * 0.25 // 1/8 rotation
+
+roof.position.y = 2.5 + 1.5 / 2
+
+
+// Door
+
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial({
+        color: 'orange'
+    })
+)
+
+door.position.y = 1 // since door texture has some space in bottom, we can not lift up completely
+door.position.z = 4 / 2 + 0.01
+
+
+
+
+houseGroup.add(wall, roof, door)
+scene.add(houseGroup)
+
+// Bushes
+
+const bushGroup = new THREE.Group()
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+const bushMaterial = new THREE.MeshStandardMaterial()
+
+const bush1 = new THREE.Mesh(
+    bushGeometry,
+    bushMaterial
+)
+
+
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+
+
+const bush2 = new THREE.Mesh(
+    bushGeometry,
+    bushMaterial
+)
+
+bush2.scale.set(0.3, 0.3, 0.3)
+bush2.position.set(1.3, 0, 2.2)
+
+
+const bush3 = new THREE.Mesh(
+    bushGeometry,
+    bushMaterial
+)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(-0.8, 0.2, 2.4)
+
+
+const bush4 = new THREE.Mesh(
+    bushGeometry,
+    bushMaterial
+)
+
+bush4.scale.set(0.2, 0.2, 0.2)
+bush4.position.set(-1.3, 0.1, 2.4)
+
+
+
+bushGroup.add(bush1, bush2, bush3, bush4)
+scene.add(bushGroup)
+const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20),
+    new THREE.MeshStandardMaterial()
+)
+
+ground.rotation.x = Math.PI * - 0.5
+scene.add(ground)
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+scene.add(ambientLight)
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+directionalLight.position.set(3, 2, -8)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 4
+camera.position.y = 2
+camera.position.z = 5
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const timer = new Timer()
+
+const tick = () => {
+    // Timer
+    timer.update()
+    const elapsedTime = timer.getElapsed()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
