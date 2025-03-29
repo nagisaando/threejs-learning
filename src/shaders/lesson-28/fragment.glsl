74b04beeca81ed97225cbd3d1f9fb209c0375e71
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-03-29 13:15:57 -0600

    learning shader pattern

--- src/shaders/lesson-28/fragment.glsl
+++ src/shaders/lesson-28/fragment.glsl
@@ -19,6 +19,52 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid)
 }
 
 
+//	Classic Perlin 2D Noise https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83#classic-perlin-noise
+//	by Stefan Gustavson (https://github.com/stegu/webgl-noise)
+//
+
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+vec2 fade(vec2 t) {return t*t*t*(t*(t*6.0-15.0)+10.0);}
+
+float cnoise(vec2 P){
+  vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+  vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+  Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+  vec4 ix = Pi.xzxz;
+  vec4 iy = Pi.yyww;
+  vec4 fx = Pf.xzxz;
+  vec4 fy = Pf.yyww;
+  vec4 i = permute(permute(ix) + iy);
+  vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+  vec4 gy = abs(gx) - 0.5;
+  vec4 tx = floor(gx + 0.5);
+  gx = gx - tx;
+  vec2 g00 = vec2(gx.x,gy.x);
+  vec2 g10 = vec2(gx.y,gy.y);
+  vec2 g01 = vec2(gx.z,gy.z);
+  vec2 g11 = vec2(gx.w,gy.w);
+  vec4 norm = 1.79284291400159 - 0.85373472095314 * 
+    vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+  g00 *= norm.x;
+  g01 *= norm.y;
+  g10 *= norm.z;
+  g11 *= norm.w;
+  float n00 = dot(g00, vec2(fx.x, fy.x));
+  float n10 = dot(g10, vec2(fx.y, fy.y));
+  float n01 = dot(g01, vec2(fx.z, fy.z));
+  float n11 = dot(g11, vec2(fx.w, fy.w));
+  vec2 fade_xy = fade(Pf.xy);
+  vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+  float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+  return 2.3 * n_xy;
+}
+
+
+
 void main()
 {
     // [pattern 1]:
@@ -221,10 +267,77 @@ void main()
 
     // [pattern 40]
     // to get the angle of a vector, use atan()
-    float angle = atan(vUv.x, vUv.y);
-    float strength = angle;
+    // float angle = atan(vUv.x, vUv.y);
+    // float strength = angle;
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
 
+    // [pattern 41]
+    // to get the angle of a vector, use atan()
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // float strength = angle;
+
+    // // [pattern 42]
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // float strength = angle;
+
+    // [pattern 43]
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // angle *= 20.0;
+    // angle = mod(angle, 1.0);
+    // float strength = angle;
+
+    // [pattern 44]
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+
+    // float strength = sin(angle * 100.0);
+
+    // [pattern 45]
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // float sinusoid = sin(angle * 100.0);
+
+    // float radius = 0.25 + sinusoid * 0.02;
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+
+    // [pattern 46] Perlin noise, http://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83 (some of them in the link don't work)
+    // which can be used to recreate nature shapes like cloud, water, fire, terrain, elevation,
+    // but it can also be used to animate the grass or snow moving in the window
+    // float strength = cnoise(vUv * 10.0);
+
+    // [pattern 47]
+    // float strength = step(0.0, cnoise(vUv * 10.0));
+
+    // [pattern 48]
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+
+    // [pattern 49]
+    // float strength = sin(cnoise(vUv * 10.0) * 20.0);
+    
+
+    // [pattern 50]
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+   
+    // clamp the strength
+    // force the value to go min 0.0 and max 1.0
+    strength = clamp(strength, 0.0, 1.0)
+    // Color version 
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    // if strength is 0, we get black
+    // if strength is 1, we get the uvColor
+    // if it's between, we get mix between those
+    // when the value goes beyond one, the color gets extrapolated. 
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
+
+    // Black and white version
+    // gl_FragColor = vec4(vec3(strength), 1.0);
 
 }
